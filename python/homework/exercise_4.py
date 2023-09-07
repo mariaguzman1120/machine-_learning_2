01edca6e937ae635d077a8e0e711fb59a9f5b16b
mariaguzman1120 <mariaguzman1120@gmail.com>
2023-09-06 22:40:44 -0500

    feat: punto 4 del taller de machine learning 2

---
+++ python/homework/exercise_4.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+
+from unsupervised.dimensionality_reduction import SVD
+from python.utils.image_processing import transform_image
+from python.metadata.path import Path
+
+# Apply SVD over the picture of your face, progressively increasing 
+# the number of singular values used.
+
+if __name__ == '__main__':
+    picture = transform_image(Path.picture)
+
+    fig, axes = plt.subplots(2, 5, figsize=(12, 6))
+
+    for j, k in enumerate(range(2, 22, 2)):
+        svd = SVD(n_components=k)
+        matrix = svd.inverse_transform(picture)
+
+        row = j // 5
+        col = j % 5
+
+        axes[row, col].imshow(matrix, cmap='gray')
+        axes[row, col].axis('off')
+        axes[row, col].set_title(f'n_components: {k}')
+
+    plt.tight_layout()
+    plt.show()
